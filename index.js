478ecb632deeb5a25c4e1ce4a4002ff6af59864c
Ole-Martin Mørk <olemartin@gmail.com>
2022-03-07 23:38:47 +0100

    Lagt på redis-støtte

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 // Require the necessary discord.js classes
 const { MessageEmbed, Client, Intents } = require("discord.js");
 const { memberNicknameMention, bold, italic } = require("@discordjs/builders");
+const { createClient } = require("redis");
+const dayjs = require("dayjs");
+
 require("dotenv").config();
 
 var token = process.env.DISCORD_TOKEN;
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const redis = createClient();
 
 // When the client is ready, run this code (only once)
-client.once("ready", () => {
+client.once("ready", async () => {
+  await redis.connect();
+
   console.log("Ready!");
 });
 
@@ -18,17 +24,39 @@ client.on("interactionCreate", async (interaction) => {
     return;
   }
 
+  const month = dayjs().format("YYYY-MM");
+
   const { commandName } = interaction;
 
-  if (commandName === "ping") {
-    await interaction.reply("Pong!");
+  if (commandName === "status") {
+    const optionMonth = interaction.options.getString("year-month");
+
+    const data = await redis.hKeys(optionMonth || month);
+
+    const response = new MessageEmbed()
+      .setColor("#0099ff")
+      .setTitle(`Treningsessions for måned: ${optionMonth || month}`);
+
+    const fields = await Promise.all(
+      data.map(async (key) => {
+        const minutes = await redis.hGet(month, key);
+        return { name: key, value: minutes };
+      })
+    );
+    response.fields = fields
+      .sort((a, b) => b.value - a.value)
+      .map((m) => ({ name: m.name, value: `${m.value} min.` }));
+
+    await interaction.reply({ embeds: [response] });
+  } else if (commandName === "status") {
+    await redis.hKeys("users");
   } else if (commandName === "register") {
     var description = interaction.options.getString("description");
     var password = interaction.options.getString("password");
     var duration = interaction.options.getInteger("duration");
 
+    await redis.HINCRBY(month, interaction.member.nickname, duration);
     const response = new MessageEmbed()
-
       .setColor("#0099ff")
       .setTitle("Treningsession opprettet 🙌")
       .setAuthor({
@@ -47,7 +75,7 @@ client.on("interactionCreate", async (interaction) => {
         { name: "Varighet", value: `${duration} minutter`, inline: true }
       );
 
-    await interaction.reply({ value: "Hello", embeds: [response] });
+    await interaction.reply({ embeds: [response] });
   }
 });
 
--- register.js
+++ register.js
@@ -1,30 +1,51 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
 
-require('dotenv').config();
+require("dotenv").config();
 
 var token = process.env.DISCORD_TOKEN;
 var clientId = process.env.CLIENT_ID;
 var guildId = process.env.GUILD_ID;
 
 const commands = [
-new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-new SlashCommandBuilder()
-    .setName('register')
-    .setDescription('Registers a trainingsession and outputs information')
-    .addIntegerOption(option => option.setName('duration').setDescription('The duration of the session in minutes').setRequired(true))
-    .addStringOption(option => option.setName('description').setDescription('Description of the training session').setRequired(true))
-    .addStringOption(option => option.setName('password').setDescription('The password of the session').setRequired(true))
-]
-.map(command => command.toJSON());
+  new SlashCommandBuilder()
+    .setName("status")
+    .setDescription("Gir status for treningsessions")
+    .addStringOption((option) =>
+      option
+        .setName("year-month")
+        .setDescription("Month for the stats, ie 2022-01 for January 2022")
+        .setRequired(false)
+    ),
+  new SlashCommandBuilder()
+    .setName("register")
+    .setDescription("Registers a trainingsession and outputs information")
+    .addIntegerOption((option) =>
+      option
+        .setName("duration")
+        .setDescription("The duration of the session in minutes")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("description")
+        .setDescription("Description of the training session")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("password")
+        .setDescription("The password of the session")
+        .setRequired(true)
+    ),
+].map((command) => command.toJSON());
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: "9" }).setToken(token);
 
-console.log(commands)
+console.log(commands);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-.then(() => console.log('Successfully registered application commands.'))
-.catch(console.error
-
-  
+rest
+  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+  .then(() => console.log("Successfully registered application commands."))
+  .catch((e) => console.error(e));
